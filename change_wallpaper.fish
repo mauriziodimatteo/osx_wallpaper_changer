2e5af4f509cfeb3d88aa5e5f35dddd3ff99e06d5
Maurizio Dimatteo <dimatteomaurizio@gmail.com>
2016-11-05 19:36:48 +0100

    first commit

---
+++ change_wallpaper.fish
@@ -0,0 +1,36 @@
+
+function change_wallpaper --description "Change wallpaper on every Desktop"
+
+set DB_PATH ~/Library/Application\ Support/Dock/desktoppicture.db
+
+	# Build SQLite commands:
+	# - Delete old data from desktoppicture.db
+	# - Insert a single row in the "data" table
+	# - Get the number of spaces
+	set SQL_CMD "DELETE FROM data; 
+				 DELETE FROM preferences;
+				 INSERT INTO data (value)
+				 	VALUES (\"$argv[1]\");
+				 SELECT COUNT(*) AS nspaces
+					FROM pictures;"
+
+	# Run SQLite and store result
+	set nspaces (sqlite3 $DB_PATH $SQL_CMD)
+
+	# Build SQlite input string
+	set input_string "(1,1,1)"
+	set i 2
+	while math "$i<=$nspaces" > /dev/null
+		set new_input 
+		set input_string $input_string ", (1,1,$i)"
+	  	set i (math "$i+1")	
+	end
+	set input_string $input_string ";"
+
+	# Add new entries to the table
+	set SQL_CMD "INSERT INTO preferences
+				 VALUES $input_string"
+	sqlite3 $DB_PATH $SQL_CMD
+
+	killall Dock
+end
